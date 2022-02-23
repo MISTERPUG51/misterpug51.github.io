c9911499f6b19a97429ecee116d66e5272360d6a
MISTERPUG51 <100051642+MISTERPUG51@users.noreply.github.com>
2022-02-22 19:34:36 -0500

    Add files via upload

---
+++ website-head.js
@@ -0,0 +1,11 @@
+function navbarbutton1click() {
+    window.location.href = "/";
+}
+
+function navbarbutton2click() {
+    window.location.href = "/random-meme-selector";
+}
+
+function navbarbutton3click() {
+    window.location.href = "/games";
+}
