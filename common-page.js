e97c0f9479d051d991f6d8c5b8ab8cda970e324f
MISTERPUG51 <100051642+MISTERPUG51@users.noreply.github.com>
2024-08-27 14:24:25 -0400

    update website

--- common-page.js
+++ common-page.js
@@ -5,5 +5,17 @@ navbarHttpRequest.onreadystatechange = function() {
        document.getElementById("navbardiv").innerHTML = navbarHttpRequest.responseText;
     }
 };
+
+var footerHttpRequest = new XMLHttpRequest();
+footerHttpRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("footer").innerHTML = document.getElementById("footer") + footerHttpRequest.responseText;
+    }
+};
+
+
 navbarHttpRequest.open("GET", "/navbar.html", true);
 navbarHttpRequest.send();
+footerHttpRequest.open("GET", "/footer.html", true);
+footerHttpRequest.send();
