26c5bf31113fae24c4dbd5ed17ff6dab913101e8
MISTERPUG51 <100051642+MISTERPUG51@users.noreply.github.com>
2025-03-01 17:31:54 -0500

    update website

--- scripts/retrieve-program-descriptions.js
+++ scripts/retrieve-program-descriptions.js
@@ -20,3 +20,14 @@ PSXControllerTestAPIHttpRequest.onreadystatechange = function() {
 };
 PSXControllerTestAPIHttpRequest.open("GET", "https://api.github.com/repos/MISTERPUG51/PSXControllerTest", true);
 PSXControllerTestAPIHttpRequest.send();
+
+var CrASHSDKHelperAPIHttpRequest = new XMLHttpRequest();
+CrASHSDKHelperAPIHttpRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        const CrASHSDKHelperJSON = JSON.parse(CrASHSDKHelperAPIHttpRequest.responseText);
+        document.getElementById("CrASHSDKHelperDescription").innerHTML = CrASHSDKHelperJSON.description;
+    }
+};
+CrASHSDKHelperAPIHttpRequest.open("GET", "https://api.github.com/repos/MISTERPUG51/CrASHSDKHelper", true);
+CrASHSDKHelperAPIHttpRequest.send();
