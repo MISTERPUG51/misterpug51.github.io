83a60635b0c65e71f84c64a3c7f120d6c6182ede
MISTERPUG51 <100051642+MISTERPUG51@users.noreply.github.com>
2025-03-02 18:13:35 -0500

    update website

--- scripts/retrieve-program-descriptions.js
+++ scripts/retrieve-program-descriptions.js
@@ -31,3 +31,14 @@ CrASHSDKHelperAPIHttpRequest.onreadystatechange = function() {
 };
 CrASHSDKHelperAPIHttpRequest.open("GET", "https://api.github.com/repos/MISTERPUG51/CrASHSDKHelper", true);
 CrASHSDKHelperAPIHttpRequest.send();
+
+var CraftMineAPIHttpRequest = new XMLHttpRequest();
+CraftMineAPIHttpRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        const CraftMineJSON = JSON.parse(CraftMineAPIHttpRequest.responseText);
+        document.getElementById("CraftMineDescription").innerHTML = CraftMineJSON.description;
+    }
+};
+CraftMineAPIHttpRequest.open("GET", "https://api.github.com/repos/MISTERPUG51/CraftMine", true);
+CraftMineAPIHttpRequest.send();
