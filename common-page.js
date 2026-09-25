69c6ba1f06fead82fd9d96a5011b966ec712157e
MISTERPUG51 <100051642+MISTERPUG51@users.noreply.github.com>
2026-09-24 23:18:22 -0400

    things

--- common-page.js
+++ common-page.js
@@ -1,29 +1,23 @@
-var navbarHttpRequest = new XMLHttpRequest();
-navbarHttpRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       document.getElementById("navbardiv").innerHTML = navbarHttpRequest.responseText;
-    }
-};
-
-var footerHttpRequest = new XMLHttpRequest();
-footerHttpRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       document.getElementById("footer").innerHTML += footerHttpRequest.responseText;
-    }
-};
-
-
-navbarHttpRequest.open("GET", "/navbar.html", true);
-navbarHttpRequest.send();
-footerHttpRequest.open("GET", "/footer.html", true);
-footerHttpRequest.send();
-
 alert("I am currently working on completely rebuilding my website as of 9/24/26. This will take a while, maybe weeks, maybe months. You can still view most of my projects on my github profile page.");
 
 
-//This is used for when Programs is clicked on the navigation bar.
-function programsButtonClicked() {
-    window.location.href="/programs";
-}
+//fetch website header and put it in the headerdiv
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("headerdiv").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "/headercontent.html", true);
+xhttp.send();
+
+
+//fetch website footer and put it in the footerdiv
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("footerdiv").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "/footercontent.html", true);
+xhttp.send();
