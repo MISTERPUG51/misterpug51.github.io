944d53cf5f1b17e48de5f381563fe4ad4beb67b2
MISTERPUG51 <100051642+MISTERPUG51@users.noreply.github.com>
2026-09-24 22:51:38 -0400

    stuff

--- scripts/dynamic-mc-background.js
+++
@@ -1,9 +0,0 @@
-const currentDate = new Date();
-let hour = currentDate.getHours();
-if (hour < 7) {
-    document.body.style.backgroundImage = "url('/images/mcbackground_night.jpg')";
-} else if (hour > 18) {
-    document.body.style.backgroundImage = "url('/images/mcbackground_night.jpg')";
-} else {
-    document.body.style.backgroundImage = "url('/images/mcbackground.jpg')";
-}
